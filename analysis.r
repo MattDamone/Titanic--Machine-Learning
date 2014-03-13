2948170de6fc982595be587346c333f028f3b7de
MattDamone <magnusohlund1988@gmail.com>
2014-03-13 19:36:49 +0100

    Started doing analysis in R

--- analysis.r
+++ analysis.r
@@ -1,3 +1,25 @@
+rm(list=ls(all=TRUE))
+source("misc.r")
+
+
 data <- read.table("Data/train.csv", sep = ",", header = T)
 
 # Läs in till array
+n <- dim(data)[1]
+
+isMale <- (data$Sex == "male")*1
+isFemale <- (data$Sex == "female")*1
+
+hasParch <- (data$Parch > 0)*1
+noParch <- (data$Parch == 0)*1
+
+hasSib <- (data$SibSp > 0)*1
+noSib <- (data$SibSp == 0)*1
+
+firstClass <- (data$Pclass == 1)*1
+secondClass <- (data$Pclass == 2)*1
+thirdClass <- (data$Pclass == 3)*1
+
+survived <- data$Survived
+
+tData <- data.frame(survived, isMale, hasParch, hasSib)
